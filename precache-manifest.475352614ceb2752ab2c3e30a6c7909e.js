92e2c513b1dfee836da690414956bcd84369e55d
Suhas Ledange <suhasledange10@gmail.com>
2022-09-11 16:59:01 +0530

    Updates

---
+++ precache-manifest.475352614ceb2752ab2c3e30a6c7909e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "335844fdc18cc8b85f7c",
+    "url": "/my-app/static/css/main.4ec55325.chunk.css"
+  },
+  {
+    "revision": "335844fdc18cc8b85f7c",
+    "url": "/my-app/static/js/main.0ec0bee6.chunk.js"
+  },
+  {
+    "revision": "cfa7322796517912f0d1",
+    "url": "/my-app/static/js/runtime~main.7a725f0a.js"
+  },
+  {
+    "revision": "d7552a1f1f052f900238",
+    "url": "/my-app/static/js/2.265c2a91.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/my-app/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "5de5a5fd6646e54844b0a9b1b560b8e6",
+    "url": "/my-app/index.html"
+  }
+];
